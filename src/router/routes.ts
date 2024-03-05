88e8f04a787067011a694cc69b986d03e798250f
Alexander Nebov <118817047+SashaNebo@users.noreply.github.com>
2024-03-05 06:48:33 +0300

    feat: add marketplace

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,8 @@
-import Home from '../pages/home/Home'
 import NotFound from '../components/notFound/NotFound'
 import Rankings from '../pages/rankings/Rankings'
 import Collection from '../pages/collection/Collection'
 import Nft from '../pages/nft/Nft'
+import Marketplace from '../pages/marketplace/Marketplace'
 
 export const rootRoute = '/nft-marketplace'
 
@@ -17,5 +17,6 @@ export const headerRoutes: ROUTE[] = [
   { path: `${rootRoute}/rankings`, component: Rankings },
   { path: `${rootRoute}/collection/:id`, component: Collection },
   { path: `${rootRoute}/nft/:id`, component: Nft },
+  { path: `${rootRoute}/marketplace`, component: Marketplace },
   { path: '*', component: NotFound },
 ]
